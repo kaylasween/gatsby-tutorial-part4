857bd9edaba0df10eaf4f15ecfbab172a12531ba
Kayla Sween <kaylasween@Kaylas-MBP.sween.home>
2019-05-12 18:35:21 -0500

    adding templates directory and adding blog post template. Adding ability to create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -13,6 +14,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
   // **Note:** The graphql function call returns a Promise
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
   return graphql(`
@@ -29,6 +31,16 @@ exports.createPages = ({ graphql, actions }) => {
     }
   `
 ).then(result => {
-    console.log(JSON.stringify(result, null, 4))
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    })
+  })
   })
 }
